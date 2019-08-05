d25228e8f441522000f7d4ed09a43af0d580e375
itwara <itwara@sina.com>
2019-08-05 19:31:11 +0800

    一次打多个线上业务包

--- config/index.js
+++ config/index.js
@@ -45,10 +45,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/' + process.env.SUB_VER + '/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../dist/' + process.env.SUB_VER + '/'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
