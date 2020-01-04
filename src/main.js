89839e10ffb6ec7a996c0f08c040e51e8303f5dc
itwara <itwara@sina.com>
2020-01-04 06:55:26 +0000

    vue原理之钩子函数：created

--- src/main.js
+++ src/main.js
@@ -33,19 +33,29 @@ new Vue({
     }
   },
   beforeCreate (e) {
-    // 初始化 event 和 钩子
-    console.log('beforeCreate: this.$options', this.$options)
-    console.log('beforeCreate: this._events', this._events)
-    console.log('beforeCreate: this', this)
+    // // 初始化 event 和 钩子
+    // console.log('beforeCreate: this.$options', this.$options)
+    // console.log('beforeCreate: this._events', this._events)
+    // console.log('beforeCreate: this', this)
 
-    // 数据未观测 (data observer) 和 event/watcher 事件未配置
-    console.log('beforeCreate: data: ', this.name)
-    console.log('beforeCreate: computed: ', this.aliasName)
-    console.log('beforeCreate: methods: ', this.setName)
-    console.log('beforeCreate: this._watchers', this._watchers)
+    // // 数据未观测 (data observer) 和 event/watcher 事件未配置
+    // console.log('beforeCreate: data: ', this.name)
+    // console.log('beforeCreate: computed: ', this.aliasName)
+    // console.log('beforeCreate: methods: ', this.setName)
+    // console.log('beforeCreate: this._watchers', this._watchers)
   },
   created (e) {
-    // console.log('created', this)
+    // $el 未创建
+    console.log('created: this.$el', this.$el)
+
+    // 数据观测 (data observer) 和 event/watcher 事件已完毕
+    console.log('created: data: ', this.name)
+    console.log('created: computed: ', this.aliasName)
+    console.log('created: methods: ', this.setName)
+    console.log('created: this._watchers', this._watchers)
+
+    // all
+    console.log('created', this)
   },
   beforeMount (e) {
     // console.log('beforeMount', this)
